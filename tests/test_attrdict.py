03d6ea1fd4b253667a336072a12212102b15962d
Al <albarrentine@gmail.com>
2024-04-03 16:49:54 -0400

    [project] adding project files

---
+++ tests/test_attrdict.py
@@ -0,0 +1,11 @@
+from communal.attrdict import AttrDict
+
+
+def test_attrdict():
+    a = AttrDict()
+    a.key = "value"
+    assert a.key == "value"
+    assert a["key"] == "value"
+
+    del a.key
+    assert "key" not in a
